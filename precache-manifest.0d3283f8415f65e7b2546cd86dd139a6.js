8348efdba82f039b8ea57958dbcea5a448b29835
emomonkey <stephen.emo@gmail.com>
2019-05-14 22:09:34 +0100

    Updates

---
+++ precache-manifest.0d3283f8415f65e7b2546cd86dd139a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b4ce1c5c058958a72473b9dbceb6dc",
+    "url": "/emomonkey/mydragdrop/index.html"
+  },
+  {
+    "revision": "43d79363bcd225566b3c",
+    "url": "/emomonkey/mydragdrop/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/emomonkey/mydragdrop/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "43d79363bcd225566b3c",
+    "url": "/emomonkey/mydragdrop/static/js/main.ec2a7003.chunk.js"
+  },
+  {
+    "revision": "8350fe31cac07fb3e44a",
+    "url": "/emomonkey/mydragdrop/static/js/runtime~main.dbd1378e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/emomonkey/mydragdrop/static/media/logo.5d5d9eef.svg"
+  }
+]);
